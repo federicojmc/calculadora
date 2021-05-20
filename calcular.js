1711675d5c62c69e04aca7f8518d7fc0871f5d3d
Federico Castro <federicojmc10@gmail.com>
2021-05-19 22:54:34 -0300

    calculadora

---
+++ calcular.js
@@ -0,0 +1,12 @@
+
+var numero_1;
+var numero_2;
+
+//sintaxis de una función
+function inicio() {
+    numero_1 = prompt("Ingrese un número:  ");
+    numero_2 = prompt("Ingrese un número:  ");
+
+    numero_1 = parseFloat(numero_1); //Transforma una cadena(texto) a un número decimal
+    numero_2 = parseInt(numero_2);
+}
